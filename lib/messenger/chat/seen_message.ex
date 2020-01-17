02d58a4b02ad95a64a705ea643ad969c54b7f732
ryanyogan <ryanyogan@gmail.com>
2020-01-17 09:31:05 -0800

    Created contexts, and schema migrations

---
+++ lib/messenger/chat/seen_message.ex
@@ -0,0 +1,21 @@
+defmodule Messenger.Chat.SeenMessage do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Messenger.Auth.User
+  alias Messenger.Chat.Message
+
+  schema "chat_seen_messages" do
+    belongs_to :user, User
+    belongs_to :message, Message
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(seen_message, attrs) do
+    seen_message
+    |> cast(attrs, [:user_id, :message_id])
+    |> validate_required([:user_id, :message_id])
+  end
+end
